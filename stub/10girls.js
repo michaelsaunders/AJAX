3d5e2be0baeab9f86fc1e6e60738dcf7e912a121
Michael Saunders <michael@michaelsaunders.com.au>
2014-05-23 00:38:37 +1000

    First part

---
+++ stub/10girls.js
@@ -0,0 +1,24 @@
+/**
+ * Created by michaelsaunders on 5/23/2014.
+ */
+{
+    "Name": "",
+    "MainPic": "/tempimages/p146078-1.jpg",
+    "Age": "27",
+    "Zodiac": "Pisces Rabbit",
+    "Weight":"114lb 52kg",
+    "Height": "170cm",
+    "Eye Color": "Blue",
+    "Hair Color":"Blonde",
+    "Marital Status": "Single",
+    "Children": "None",
+    "Religion": "Christian (Orthodox)",
+    "Smoker": "No",
+    "Drinker": "Socially",
+    "Education": "University",
+    "Company":"Private",
+    "Job Title":"Florist",
+    "Sports":"Fitness Yoga",
+    "Hobbies": "Films",
+    "English": "Fair"
+}
